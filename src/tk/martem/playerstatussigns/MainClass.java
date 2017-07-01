56bf253e072c830ff76c28932ff2b7afc66b4e24
MartenM <marten.struijk@gmail.com>
2017-07-01 12:28:35 +0200

    First initial test.

--- src/tk/martem/playerstatussigns/MainClass.java
+++ src/tk/martem/playerstatussigns/MainClass.java
@@ -1,7 +1,14 @@
 package tk.martem.playerstatussigns;
 
+import com.earth2me.essentials.Essentials;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import tk.martem.playerstatussigns.commands.PlayerStatusSignsCommand;
+import tk.martem.playerstatussigns.events.OnSignChangeEvent;
+import tk.martem.playerstatussigns.objects.PlayerStatusSign;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 /**
@@ -12,20 +19,57 @@ import java.util.logging.Logger;
 public class MainClass extends JavaPlugin {
 
     private final Logger logger = getLogger();
+    public ArrayList<PlayerStatusSign> signs;
+    public Essentials essentials;
 
     public void onEnable(){
 
+        signs = new ArrayList<>();
 
+        registerConfigs();
+        registerCommands();
+        registerEvents();
+
+        loadSigns();
+
+        if(Bukkit.getPluginManager().isPluginEnabled("Essentials")){
+            essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
+            logger.info("Essentials plugin was found. Using it to determine if someone is AFK.");
+        }
+        else{
+            logger.info("Essentials was not found. Cannot determine AFK.");
+        }
 
         logger.info("Enabled player status signs successfully.");
     }
 
     public void onDisable(){
+        saveSigns();
 
-
-
-        logger.info("Disabled player status signs succesfully.");
+        logger.info("Disabled player status signs successfully.");
     }
 
+
+
+    public void registerConfigs(){
+        saveDefaultConfig();
+    }
+
+    public void registerEvents(){
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new OnSignChangeEvent(this), this);
+    }
+
+    public void registerCommands(){
+        getCommand("playerstatussigns").setExecutor(new PlayerStatusSignsCommand(this));
+    }
+
+    public void loadSigns(){
+
+    }
+
+    public void saveSigns(){
+
+    }
 
 }
